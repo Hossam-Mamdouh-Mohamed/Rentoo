dec6bc8a4140fba8a62c9463758d8676b4d606aa
Hossam <Hossam@DESKTOP-P0Q2AGT>
2025-04-20 19:29:49 +0200

    Some_Updates

---
+++ Rentoo.Web/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rentoo.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
