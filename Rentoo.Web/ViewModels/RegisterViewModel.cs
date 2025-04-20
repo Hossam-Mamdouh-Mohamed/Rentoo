97c5612a4eb4838b260eb4eaf2819d975f45ecfe
Hossam <Hossam@DESKTOP-P0Q2AGT>
2025-04-21 01:56:06 +0200

    Update_Reg

--- Rentoo.Web/ViewModels/RegisterViewModel.cs
+++ Rentoo.Web/ViewModels/RegisterViewModel.cs
@@ -6,9 +6,14 @@ namespace web.ViewModels
     {
         [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Range(10,120)]
         public int Age { get; set; }
         public string Address { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
         public string UserName { get; set; }
         [Required]
         [DataType(DataType.Password)]
@@ -18,7 +23,7 @@ namespace web.ViewModels
         [Compare("Password", ErrorMessage = "Password and Confirm Password must match")]
         public string ConfirmPassword { get; set; }
         [Required]
-        public string Role { get; set; } // Add this property
+        public string Role { get; set; } 
         public string PhoneNumber { get; set; }
     }
 }
